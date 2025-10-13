4beb1ab8afe093bbc58c309032725982a8a61872
simbapc <251058094@qq.com>
2025-10-14 01:13:42 +0800

    m error

--- gss-backend/routes/topicRoutes.js
+++ gss-backend/routes/topicRoutes.js
@@ -7,11 +7,10 @@ const { protect, isTeacher, isStudent } = require("../middleware/authMiddleware"
 router.get('/available', protect, isStudent, topicController.getAvailableTopics);
 
 // 所有课题相关的路由都需要登录，并且是教师角色
-router.use(protect, isTeacher);
 
-router.post("/", topicController.createTopic); // 创建课题
-router.get("/my-topics", topicController.getTeacherTopics); // 获取我的课题
-router.put("/:id", topicController.updateTopic); // 更新课题
-router.delete("/:id", topicController.deleteTopic); // 删除课题
+router.post("/", protect, isTeacher,topicController.createTopic); // 创建课题
+router.get("/my-topics", protect, isTeacher, topicController.getTeacherTopics); // 获取我的课题
+router.put("/:id", protect, isTeacher,topicController.updateTopic); // 更新课题
+router.delete("/:id", protect, isTeacher,topicController.deleteTopic); // 删除课题
 
 module.exports = router;
